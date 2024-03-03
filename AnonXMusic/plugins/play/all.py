27f3a1a87810ca9dfb01dca3097eeb872c750acb
TOXIIIICO <156939831+TOXIIIICO@users.noreply.github.com>
2024-03-03 03:55:38 +0200

    Update all.py

--- AnonXMusic/plugins/play/all.py
+++ AnonXMusic/plugins/play/all.py
@@ -8,11 +8,11 @@ from pyrogram import filters
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from AnonXMusic  import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonXMusic  import app
+from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonXMusic import app
+from telegraph import upload_file
 from asyncio import gather
 from pyrogram.errors import FloodWait
-
 
 
 
