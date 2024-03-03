66a1d38e0a68d0a8450d0aac038c77d1763737ff
TOXIIIICO <156939831+TOXIIIICO@users.noreply.github.com>
2024-03-03 02:55:39 +0200

    Add files via upload

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -4,14 +4,15 @@ import importlib
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
+
 import config
 from AnonXMusic import LOGGER, app, userbot
 from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import sudo
+from AnonXMusic.plugins.play.azan import azan
 from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-from AnonXMusic.plugins.play.azan import azkar
 
 
 async def init():
@@ -40,15 +41,7 @@ async def init():
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Anony.start()
-    try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except NoActiveGroupCall:
-        LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
-        )
-        exit()
-    except:
-        pass
+    
     await Anony.decorators()
     LOGGER("AnonXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
@@ -56,7 +49,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    await azkar()
+    await azan()
     LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
 
